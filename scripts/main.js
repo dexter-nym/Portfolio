39376d95e7d000c25ad32e8fcd1754e2e6a5bc5a
Kunal Paraye <kunalparaye39@gmail.com>
2025-02-12 15:09:31 +0530

    99% finished commit

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const circle = document.querySelector(".circle-hover");
+circle.addEventListener("mousemove", function (e) {
+  let x = e.pageX - circle.offsetLeft;
+  let y = e.pageY - circle.offsetTop;
+
+  circle.style.setProperty("--x", x + "px");
+  circle.style.setProperty("--y", y + "px");
+});
+
+const cursor = document.querySelector(".cursor");
+
+circle.addEventListener("mousemove", function (e) {
+  let x = e.pageX - circle.offsetLeft;
+  let y = e.pageY - circle.offsetTop;
+
+  circle.style.setProperty("--xx", x + "px");
+  circle.style.setProperty("--yy", y + "px");
+});
